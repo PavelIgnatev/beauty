f6a284a2a69f53f453ad8888d0134af714cfd5ab
Pavel <palllkaignatev@gmail.com>
2021-04-10 01:47:55 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <div @click="geoPosition">GEOLOCATION</div>
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    geoPosition() {
+      if (!navigator.geolocation) {
+        console.log("Не поддерживается ваш браузер");
+        return;
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error, {
+        enableHighAccuracy: true,
+      });
+
+      function success({ coords }) {
+        const { latitude, longitude } = coords;
+        const position = [latitude, longitude];
+        console.log(position);
+      }
+
+      function error({ message }) {
+        console.log(message);
+      }
+    },
+  },
+};
+</script>
+<style lang="scss"></style>
